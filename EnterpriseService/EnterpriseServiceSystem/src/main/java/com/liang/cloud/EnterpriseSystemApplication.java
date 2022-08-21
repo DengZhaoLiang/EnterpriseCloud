958e684058a10a34812736187730262dfaae591f
Liang <386229680@qq.com>
2022-08-21 15:33:35 +0800

    集成SpringCloud+SpringBoot

---
+++ EnterpriseService/EnterpriseServiceSystem/src/main/java/com/liang/cloud/EnterpriseSystemApplication.java
@@ -0,0 +1,17 @@
+package com.liang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author Liang
+ * 2022-08-21
+ */
+@SpringBootApplication
+public class EnterpriseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseSystemApplication.class, args);
+    }
+}
