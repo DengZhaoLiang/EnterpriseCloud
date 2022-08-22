ab769195070787f1e2bb68fc8832b28464c82ed3
Liang <386229680@qq.com>
2022-08-22 23:30:31 +0800

    使用注解校验微服务消息参数

--- EnterpriseService/EnterpriseServiceSystem/src/main/java/com/liang/service/system/controller/SystemController.java
+++ EnterpriseService/EnterpriseServiceSystem/src/main/java/com/liang/service/system/controller/SystemController.java
@@ -1,13 +1,18 @@
 package com.liang.service.system.controller;
 
 import com.liang.common.entity.Result;
+import com.liang.service.system.Dto.SystemDTO;
 import com.liang.service.system.service.ISystemService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/system")
@@ -33,5 +38,11 @@ public class SystemController {
     @ApiOperation(value = "自定义异常及返回测试接口")
     public Result<String> exception() {
         return Result.data(systemService.exception());
+    }
+
+    @PostMapping(value = "valid")
+    @ApiOperation(value = "参数校验测试接口")
+    public Result<SystemDTO> valid(@Valid @RequestBody SystemDTO systemDTO) {
+        return Result.data(systemDTO);
     }
 }
