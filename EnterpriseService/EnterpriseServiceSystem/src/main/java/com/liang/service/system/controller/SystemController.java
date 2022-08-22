4b1d0bcae07f315abd03f96df2f1384bb21142de
Liang <386229680@qq.com>
2022-08-23 00:33:30 +0800

    使用Nacos分布式配置中心

--- EnterpriseService/EnterpriseServiceSystem/src/main/java/com/liang/service/system/controller/SystemController.java
+++ EnterpriseService/EnterpriseServiceSystem/src/main/java/com/liang/service/system/controller/SystemController.java
@@ -5,7 +5,10 @@ import com.liang.service.system.Dto.SystemDTO;
 import com.liang.service.system.service.ISystemService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,11 +19,15 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/system")
-@AllArgsConstructor
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Api(tags = "EnterpriseServiceSystem")
+@RefreshScope
 public class SystemController {
 
     private final ISystemService systemService;
+
+    @Value("${spring.datasource.maxActive}")
+    private String nacosMaxActiveType;
 
     @GetMapping(value = "/list")
     @ApiOperation(value = "system list接口")
@@ -34,15 +41,21 @@ public class SystemController {
         return systemService.page();
     }
 
-    @GetMapping(value = "exception")
+    @GetMapping(value = "/exception")
     @ApiOperation(value = "自定义异常及返回测试接口")
     public Result<String> exception() {
         return Result.data(systemService.exception());
     }
 
-    @PostMapping(value = "valid")
+    @PostMapping(value = "/valid")
     @ApiOperation(value = "参数校验测试接口")
     public Result<SystemDTO> valid(@Valid @RequestBody SystemDTO systemDTO) {
         return Result.data(systemDTO);
+    }
+
+    @PostMapping(value = "/nacos")
+    @ApiOperation(value = "Nacos读取配置文件测试接口")
+    public Result<String> nacos() {
+        return Result.data(nacosMaxActiveType);
     }
 }
